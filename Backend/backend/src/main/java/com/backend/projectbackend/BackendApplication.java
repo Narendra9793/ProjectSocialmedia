32761702cdb6ddf25e51baf6432825a1b2e80902
Narendra Pratap Singh <72899015+Narendra9793@users.noreply.github.com>
2024-08-07 14:50:11 +0530

    some changes

--- Backend/backend/src/main/java/com/backend/projectbackend/BackendApplication.java
+++ Backend/backend/src/main/java/com/backend/projectbackend/BackendApplication.java
@@ -3,10 +3,12 @@ package com.backend.projectbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.backend.projectbackend")
-
+@ComponentScan(basePackages = "com.backend.projectbackend")
+@EnableJpaRepositories(basePackages = "com.backend.projectbackend.Dao")
 public class BackendApplication {
 
 	public static void main(String[] args) {
