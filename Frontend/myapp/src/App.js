d5a92a31a8098c0e07167773fc16386fe372bad9
Narendra Pratap Singh <72899015+Narendra9793@users.noreply.github.com>
2025-01-03 17:39:58 +0530

    made responsive

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -8,13 +8,36 @@ import SignUp from './Components/SignUp/SignUp';
 import Feeds from './Components/Feeds/Feeds';
 import { SocketProvider } from './context/SocketProvider';
 import { UserProvider } from './context/UserProvider';
+import Sidebar from './Components/Sidebar/Sidebar';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [showSidemenu, setSidemenu] = useState(false);
+  const [showNavbar, setShownavbar] = useState(window.innerWidth > 700);
+
+  const handlesidemenu= ()=>{
+    setSidemenu(!showSidemenu)
+  }
+  useEffect(()=>{
+    window.addEventListener('resize',()=>{
+      setShownavbar(window.innerWidth > 700)
+    })
+
+    return ()=>{
+      window.removeEventListener('resize',()=>{
+        setShownavbar(window.innerWidth > 700)
+      })
+    }
+  }, [])
   return (
     <UserProvider>
       <SocketProvider>
         <BrowserRouter>
-          <Navbar />
+         { showNavbar && <Navbar className="navbar"/>}
+          <div className="burger-menu">
+           <button type="button" onClick={handlesidemenu}><i class="fa fa-bars" aria-hidden="true"></i></button>
+          </div>
+          {showSidemenu && <Sidebar/>}
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
--- Frontend/myapp/src/Components/Navbar/Navbar.jsx
+++ Frontend/myapp/src/Components/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
       <div className=" fixed top-0 left-0 z-50 flex w-full h-[10vh] text-black font-thin text-xl  bg-contain bg-center   justify-between items-center "  style={{ backgroundImage: `url(${backgroundImg})` }}>
         <Link className="ml-10 bg-purple-500 p-1 rounded-md" to="/home">Home</Link>
         <div className="flex gap-x-20 mr-10 ">
-            <Link className= " bg-purple-500 p-1 rounded-md" to="/user/profile" >Profile</Link>
-            <Link className= " bg-purple-500 p-1 rounded-md" to="/user/feeds">Feeds </Link>
-            <Link className= " bg-purple-500 p-1 rounded-md" to="/login">Login</Link>
-            <Link className= " bg-purple-500 p-1 rounded-md" to="/signUp">Sign Up</Link>
+            <Link className= " bg-purple-500 p-1 rounded-md " to="/user/profile" >Profile</Link>
+            <Link className= " bg-purple-500 p-1 rounded-md " to="/user/feeds">Feeds </Link>
+            <Link className= " bg-purple-500 p-1 rounded-md " to="/login">Login</Link>
+            <Link className= " bg-purple-500 p-1 rounded-md " to="/signUp">Sign Up</Link>
         </div>
       </div>
     </>
