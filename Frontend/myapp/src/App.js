feed0bd91909ec02f6992cf7fa0fa5049c81c5d9
Narendra Pratap Singh <72899015+Narendra9793@users.noreply.github.com>
2025-04-22 23:31:25 +0530

    Lazzy loading added in App.js

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './Components/Login/Login';
-import Home from './Components/Home/Home';
-import Profile from './Components/Profile/Profile';
-import SignUp from './Components/SignUp/SignUp';
-import Feeds from './Components/Feeds/Feeds';
 import { SocketProvider } from './context/SocketProvider';
 import { UserProvider } from './context/UserProvider';
 import Sidebar from './Components/Sidebar/Sidebar';
 import { useEffect, useState } from 'react';
+import React, { Suspense, lazy } from 'react';
+
+const Home = lazy(() => import('./Components/Home/Home'));
+const Profile = lazy(() => import('./Components/Profile/Profile'));
+const SignUp = lazy(() => import('./Components/SignUp/SignUp'));
+const Feeds = lazy(() => import('./Components/Feeds/Feeds'));
+const Login = lazy(() => import('./Components/Login/Login'));
+
 
 function App() {
   const [showSidemenu, setSidemenu] = useState(false);
@@ -33,19 +36,21 @@ function App() {
     <UserProvider>
       <SocketProvider>
         <BrowserRouter>
-         { showNavbar && <Navbar className="navbar"/>}
-          <div className="burger-menu">
-           <button type="button" onClick={handlesidemenu}><i class="fa fa-bars" aria-hidden="true"></i></button>
-          </div>
-          {showSidemenu && <Sidebar/>}
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/user/profile" element={<Profile />} />
-              <Route path="/user/feeds" element={<Feeds />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signUp" element={<SignUp />} />
-            </Routes>
+          <Suspense fallback={<div>Loading Page...</div>}>
+            { showNavbar && <Navbar className="navbar"/>}
+              <div className="burger-menu">
+              <button type="button" onClick={handlesidemenu}><i class="fa fa-bars" aria-hidden="true"></i></button>
+              </div>
+              {showSidemenu && <Sidebar/>}
+              <Routes>  
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/user/profile" element={<Profile />} />
+                <Route path="/user/feeds" element={<Feeds />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signUp" element={<SignUp />} />
+              </Routes>
+          </Suspense>
         </BrowserRouter>
       </SocketProvider>
     </UserProvider>
